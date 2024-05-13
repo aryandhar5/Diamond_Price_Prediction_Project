c61a33ce58c994b13250481ec5e63c42ac1a325c
aryandhar18 <aryandhar18@gmail.com>
2024-05-14 01:56:27 +0530

    final commit

--- src/Compnents/Data_ingestion.py
+++ src/Compnents/Data_ingestion.py
@@ -11,30 +11,29 @@ from dataclasses import dataclass
 ## initialize the data ingestion config
 
 @dataclass
-class DataTngestionConfig:
-    train_data_path:str=os.path.join('artifacts','train.csv')
-    test_data_path:str=os.path.join('artifacts','test.csv')
-    raw_data_path:str=os.path.join('artifacts','raw.csv')
+class DataIngestionconfig:
+    train_data_path=os.path.join('artifacts','train.csv')
+    test_data_path=os.path.join('artifacts','test.csv')
+    raw_data_path=os.path.join('artifacts','raw.csv')
 
 
-## create data ingestion class
-
+## create a data ingestion class
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataTngestionConfig
+        self.ingestion_config=DataIngestionconfig()
 
     def initiate_data_ingestion(self):
-        logging.info('Data ingestion method starts')
+        logging.info('Data Ingestion method starts')
 
         try:
-            df=pd.read_csv(os.path.join('notebooks/Data','gemstone.csv'))
-            logging.info('dataset read as pandas Dataframe')
+            df=pd.read_csv(os.path.join('notebooks/data','gemstone.csv'))
+            logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path,index=False)
 
-            logging.info('Raw data is created')
-
+            logging.info("Train test split")
             train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
@@ -45,15 +44,13 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-            )
 
-            
+            )
 
 
 
         except Exception as e:
-            logging.info('Excpetion occured at Data ingestion stage')
-            raise CustomException(e,sys)
+            logging.info('Error occured in Data Ingestion config')
 
 
 
